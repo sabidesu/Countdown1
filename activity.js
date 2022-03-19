20b910546f167842f1e0ca8eabdef43e806c0a55
julian donald <zentixzaneic@gmail.com>
2022-03-19 14:50:27 -0400

    fix indentation

--- activity.js
+++ activity.js
@@ -11,47 +11,47 @@ const bookList = [
     { title: "Ulysses", author: "James Joyce" },
     { title: "The Great Gatsby", author: "F. Scott Fitzgerald" },
     { title: "Moby Dick", author: "Herman Melville" },
-  ];
+];
 
-  /* Intermediate Challenge */
-  
-  /* (4) Write a one-line arrow function that takes in a number and returns a string stating whether the
-    number is positive or negative using a ternary operator (assume the number will never be zero) */
-  
-  /* (5) Write a switch statement for a 'day' variable that prints out something based off of what day of
-    the week it is
-    i.e. if it's Monday, print "good luck" or wednesday print "hump day" or friday print "party" */
-  
-  /* (6) Write an arrow function that takes in a number, and uses a for loop to return the sum of every
-    number from 1 up to that number
-    ex. sumUp(7) = 28 */
-  
-  /* Harder Challenge */
+/* Intermediate Challenge */
 
-  /* (7) Write an arrow function that converts the temperature from Celsius to Fahrenheit and then tells
-    me what I should wear accordingly */
-  
-  /* (8) Write a function that takes in an array and prints out the amount of truthy values in that array
-    using .forEach() */
-  
-  const exampleArray = ["Hello, world!", 8, null, false, "", "0", -22];
-  
-  /* (9) Using the map function and arrow syntax, return an array of object that contain a fullName field
-    and an averageGrade field representing the letter grade that corresponds to their GPA */
-  
-  const attendance = [
-    { firstName: "Clay", lastName: "Tondreau", gpa: 4.0 },
-    { firstName: "Tucker", lastName: "Wilson", gpa: 2.0 },
-    { firstName: "Eliza", lastName: "Tobin", gpa: 3.7 },
-    { firstName: "Sofia", lastName: "Ackerman", gpa: 1.1 },
-    { firstName: "Thomas", lastName: "Beddow", gpa: 2.3 },
-    { firstName: "Jackson", lastName: "Wolf", gpa: 4.0 },
-    { firstName: "Jared", lastName: "Nguyen", gpa: 4.0 },
-  ];
+/* (4) Write a one-line arrow function that takes in a number and returns a string stating whether the
+number is positive or negative using a ternary operator (assume the number will never be zero) */
 
-  /* Hardest Challenge (Don't do this without completing harder challenges) */
+/* (5) Write a switch statement for a 'day' variable that prints out something based off of what day of
+the week it is
+i.e. if it's Monday, print "good luck" or wednesday print "hump day" or friday print "party" */
 
-  /* Write a function that solves the "every number eventually equals 4" puzzle. The output should be
-    an array of the path you took to make it equal four
-    ex/ [11, 6, 3, 5, 4], [19, 8, 5, 4] or [252, 18, 8, 5, 4]
-    For context: https://puzzling.stackexchange.com/questions/29137/every-number-eventually-equals-4 */
+/* (6) Write an arrow function that takes in a number, and uses a for loop to return the sum of every
+number from 1 up to that number
+ex. sumUp(7) = 28 */
+
+/* Harder Challenge */
+
+/* (7) Write an arrow function that converts the temperature from Celsius to Fahrenheit and then tells
+me what I should wear accordingly */
+
+/* (8) Write a function that takes in an array and prints out the amount of truthy values in that array
+using .forEach() */
+
+const exampleArray = ["Hello, world!", 8, null, false, "", "0", -22];
+
+/* (9) Using the map function and arrow syntax, return an array of object that contain a fullName field
+and an averageGrade field representing the letter grade that corresponds to their GPA */
+
+const attendance = [
+{ firstName: "Clay", lastName: "Tondreau", gpa: 4.0 },
+{ firstName: "Tucker", lastName: "Wilson", gpa: 2.0 },
+{ firstName: "Eliza", lastName: "Tobin", gpa: 3.7 },
+{ firstName: "Sofia", lastName: "Ackerman", gpa: 1.1 },
+{ firstName: "Thomas", lastName: "Beddow", gpa: 2.3 },
+{ firstName: "Jackson", lastName: "Wolf", gpa: 4.0 },
+{ firstName: "Jared", lastName: "Nguyen", gpa: 4.0 },
+];
+
+/* Hardest Challenge (Don't do this without completing harder challenges) */
+
+/* Write a function that solves the "every number eventually equals 4" puzzle. The output should be
+an array of the path you took to make it equal four
+ex/ [11, 6, 3, 5, 4], [19, 8, 5, 4] or [252, 18, 8, 5, 4]
+For context: https://puzzling.stackexchange.com/questions/29137/every-number-eventually-equals-4 */
